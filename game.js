8562fe73179bad57caa3b8ed1acecdb401a1e435
Nick Cheung <nickhcheung@hotmail.com>
2017-07-31 22:07:39 -0700

    Add deflection ability to ball

--- game.js
+++ game.js
@@ -22,14 +22,37 @@ function Paddle(x, y, width, height){
   this.width = width;
   this.height = height;
   this.speed = 10;
+  // top and bottom edges for detecting collisions
+  this.edge = {
+    top: this.y,
+    bottom: this.y + this.height,
+    right: this.x + this.width,
+    left: this.x
+  };
 
   this.move = function(dy){
     context.clearRect(this.x, this.y, this.width, this.height);
     this.y += dy;
+    this.edge.top += dy;
+	  this.edge.bottom += dy;
   };
   this.render = function(){
     context.fillRect(this.x, this.y, this.width, this.height);
   };
+};
+
+//Random negative function
+function randomizeValue(num){
+  var randomNegative = [1, -1];
+  num *= randomNegative[Math.floor(Math.random() * randomNegative.length)];
+  return num;
+};
+
+//Randomizes Y value
+function randomBallY(){
+  var y_set = [1, 3];
+  var num = y_set[Math.floor(Math.random() * y_set.length)];
+  return num;
 };
 
 //Ball
@@ -38,7 +61,51 @@ function Ball(){
   this.y = 150;
   this.width = 5;
   this.height = 5;
+	this.edge = {
+		right: this.x + 5,
+		left: this.x,
+		top: this.y,
+		bottom: this.y + 5
+  };
 
+  this.speed_x = randomizeValue(3);
+  this.speed_y = randomizeValue(randomBallY());
+
+  this.move = function(player, computer){
+    context.clearRect(this.x, this.y, this.width, this.height);
+    this.x += this.speed_x;
+    this.y += this.speed_y;
+
+    this.edge.left += this.speed_x;
+  	this.edge.right += this.speed_x;
+  	this.edge.top += this.speed_y;
+  	this.edge.bottom += this.speed_y;
+
+    //Collision with Player paddle
+    if(this.edge.left < player.edge.right && this.edge.left > player.edge.right - 2){
+      if(this.edge.top < player.edge.bottom && this.edge.bottom > player.edge.top){
+        this.speed_x = -this.speed_x;
+      } else{
+        console.log('game over');
+      };
+    };
+    //Collision with Computer paddle
+    if(this.edge.right === computer.edge.left){
+      if(this.edge.top < computer.edge.bottom && this.edge.bottom > computer.edge.top){
+        this.speed_x = -this.speed_x;
+      } else{
+        console.log('game over');
+      };
+    };
+    // Collision with top boundary
+    if(this.edge.top === 0){
+      this.speed_y = -this.speed_y;
+    };
+    // Collision with bottom boundary
+    if(this.edge.bottom >= 300){
+      this.speed_y = -this.speed_y;
+    };
+  };
   this.render = function(){
     context.fillRect(this.x, this.y, this.width, this.height);
   };
@@ -71,6 +138,10 @@ var player = new Paddle(10, 130, 10, 50);
 var computer = new Paddle(480, 130, 10, 50);
 var ball = new Ball();
 
+var update = function(){
+  ball.move(player, computer);
+};
+
 function render(){
   drawBox();
   player.render();
@@ -88,7 +159,10 @@ var animate = window.requestAnimationFrame ||
 		window.setTimeout(callback, 1000 / 60);
 	};
 
+var endAnimate = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+
 function step(){
+  update();
   render();
   //Initialize Animation
   animate(step);
