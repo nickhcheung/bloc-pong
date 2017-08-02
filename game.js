ddc8777019a090293cea610f291071e773c70679
Nick Cheung <nickhcheung@hotmail.com>
2017-08-01 23:29:59 -0700

    Add Computer AI (ball tracking). Add game reset upon scoring.

--- game.js
+++ game.js
@@ -39,19 +39,25 @@ function Paddle(x, y, width, height){
   this.render = function(){
     context.fillRect(this.x, this.y, this.width, this.height);
   };
+  //Computer AI function
+  this.computerUpdate = function(ball){
+    //Computer AI only moves if the ball is headed in it's direction
+    if(ball.speed_x > 0){
+      //Goes up if ball is above it
+      if(ball.y >= this.edge.top){
+        computer.move(Math.abs(ball.speed_y));
+      }//And down if ball is below
+      else if(ball.y <= this.edge.top){
+        computer.move(-Math.abs(ball.speed_y));
+      };
+    };
+  };
 };
 
 //Random negative function
 function randomizeValue(num){
   var randomNegative = [1, -1];
   num *= randomNegative[Math.floor(Math.random() * randomNegative.length)];
-  return num;
-};
-
-//Randomizes Y value
-function randomBallY(){
-  var y_set = [1, 3];
-  var num = y_set[Math.floor(Math.random() * y_set.length)];
   return num;
 };
 
@@ -69,7 +75,7 @@ function Ball(){
   };
 
   this.speed_x = randomizeValue(3);
-  this.speed_y = randomizeValue(randomBallY());
+  this.speed_y = randomizeValue(1);
 
   this.move = function(player, computer){
     context.clearRect(this.x, this.y, this.width, this.height);
@@ -85,16 +91,12 @@ function Ball(){
     if(this.edge.left < player.edge.right && this.edge.left > player.edge.right - 2){
       if(this.edge.top < player.edge.bottom && this.edge.bottom > player.edge.top){
         this.speed_x = -this.speed_x;
-      } else{
-        console.log('game over');
       };
     };
     //Collision with Computer paddle
     if(this.edge.right === computer.edge.left){
       if(this.edge.top < computer.edge.bottom && this.edge.bottom > computer.edge.top){
         this.speed_x = -this.speed_x;
-      } else{
-        console.log('game over');
       };
     };
     // Collision with top boundary
@@ -105,9 +107,53 @@ function Ball(){
     if(this.edge.bottom >= 300){
       this.speed_y = -this.speed_y;
     };
+    //CPU scores
+    if(this.x < 0){
+      console.log('CPU Scores');
+      resetTable();
+    };
+    //Player scores
+    if(this.x > 500){
+      console.log('Player Scores');
+      resetTable();
+    };
   };
   this.render = function(){
     context.fillRect(this.x, this.y, this.width, this.height);
+  };
+};
+
+function resetTable(){
+  //Ball reset
+  ball.x = 250;
+  ball.y = 150;
+  ball.speed_x = randomizeValue(3);
+  ball.speed_y = randomizeValue(1);
+  ball.edge = {
+		right: ball.x + 5,
+		left: ball.x,
+		top: ball.y,
+		bottom: ball.y + 5
+  };
+  //Player reset
+  context.clearRect(player.x, player.y, player.width, player.height);
+  player.x = 10;
+  player.y = 130;
+  player.edge = {
+    top: player.y,
+    bottom: player.y + player.height,
+    right: player.x + player.width,
+    left: player.x
+  };
+  //CPU reset
+  context.clearRect(computer.x, computer.y, computer.width, computer.height);
+  computer.x = 480;
+  computer.y = 130;
+  computer.edge = {
+    top: computer.y,
+    bottom: computer.y + computer.height,
+    right: computer.x + computer.width,
+    left: computer.x
   };
 };
 
@@ -140,6 +186,7 @@ var ball = new Ball();
 
 var update = function(){
   ball.move(player, computer);
+  computer.computerUpdate(ball);
 };
 
 function render(){
@@ -158,8 +205,6 @@ var animate = window.requestAnimationFrame ||
 	function(callback){
 		window.setTimeout(callback, 1000 / 60);
 	};
-
-var endAnimate = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
 
 function step(){
   update();
